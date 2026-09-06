5cb54b6b1822241b15f3512643d1810d71c26bff
Sarvesh M <msarvesh.dav@gmail.com>
2026-09-06 17:24:13 +0530

    chore: remove unused departments compatibility wrapper

--- components/Departments.jsx
+++
@@ -1,9 +0,0 @@
-export { default } from "./AllDepartments";
-export function ReviewCard({ name, body }) {
-  return (
-    <figure>
-      <figcaption>{name}</figcaption>
-      <blockquote>{body}</blockquote>
-    </figure>
-  );
-}
